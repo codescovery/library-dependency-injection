7fadbf7fffe456dac7f65b8116cbf7f3d10b3bf0
Vinicius Andrade <vinicius.andrade00@gmail.com>
2023-09-20 04:57:47 -0300

    adding result builder extensions

---
+++ src/Package/Codescovery.Library.DependencyInjection/Extensions/ResultBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Codescovery.Library.Commons.Builders;
+using Codescovery.Library.Commons.Interfaces.Result;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Codescovery.Library.DependencyInjection.Extensions
+{
+    public static class ResultBuilderExtensions
+    {
+
+        public static IServiceCollection AddResultBuilderService(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            services.Add(ResultBuilder.Default(),serviceLifetime);
+            return services;
+        }
+        public static IServiceCollection AddResultBuilderService<T>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        where T : class, IResultBuilderService
+        {
+            services.Add(ResultBuilder.Default(), serviceLifetime);
+            return services;
+        }
+    }
+}
